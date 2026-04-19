55900a8c9b315fecfd34b4d86f42daaeb6ba8c72
Хасиев Aлександр <westalex777@yandex.ru>
2026-04-19 19:32:47 +0300

    Subgraph dev

---
+++ src/main/java/org/graph/api/core/subgraph/Subgraph.java
@@ -0,0 +1,8 @@
+package org.graph.api.core.subgraph;
+
+import org.graph.api.core.GraphState;
+import org.graph.api.core.node.Node;
+
+public interface Subgraph<S extends GraphState> extends Node<S> {
+
+}
